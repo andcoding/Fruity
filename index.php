dc9346d31ed157e66b9ccb8ad2b538f2ffdf4cbd
Yordy <noelnew90@gmail.com>
2020-09-29 18:55:20 -0400

    This is the section header commit

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
+<!DOCTYPE html>
+<html>
 
+<head>
+
+    <title>Fruity Project</title>
+
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+    <div class="container">
+
+        <header>
+
+            <img src="images/logo_fruity.png" class="logo" />
+
+            <input type="search" class="input-search" placeholder="Search Here">
+
+        </header>
+    </div>
+
+</body>
+</html>
