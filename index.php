797a5a7647f52acf2f83a9a4e883247f01320e7a
Yordy <noelnew90@gmail.com>
2020-10-26 21:36:26 -0400

    class and images renamed correctly

--- index.php
+++ index.php
@@ -32,32 +32,32 @@
             <nav>
                 <ul class="navbar">
                     <li><a href="#about">About</a></li>
-                    <li><a href="#seasonal" class="seasonal">Seasonal</a></li>
+                    <li><a href="#seasonal" class="active">Seasonal</a></li>
                     <li><a href="#category">Category</a></li>
 
                 </ul>
             </nav>
 
-            <div class="fruit_definition">
+            <div class="fruit-definition">
                 <h1>Pineapple</h1>
                 <p>The pineapple is a tropical plant with an edible fruit, also called a pineapple and the most economically significant plant in the family Bromeliaceae.</p>
 
             </div>
 
-            <div class="center-image">
+            <div class="center_image">
                 <img src="images/pineapple.png" alt="pineapple">
             </div>
 
             <div class="fruit-facts">
 
-                <div class="health_benefits">
-                    <img class="straight-line" src="images/straight-line.png" alt="straight red line">
+                <div class="health-benefits">
+                    <img class="straight_line" src="images/straight_line.png" alt="straight red line">
                     <h2>Health Benefits</h2>
                     <p>Pineapple contains significant amount of vitamin C. a water-soluble antioxidant that fights cell damage.</p>
                 </div>
 
-                <div class="nutrion_values">
-                    <img class="l-line" src="images/l-line.png" alt="L line">
+                <div class="nutrion-values">
+                    <img class="l_line" src="images/l_line.png" alt="L line">
                     <h2>Nutriotional Values</h2>
                     <li>Calories 74</li><br>
                     <li>Sugars 13.7 g</li><br>
